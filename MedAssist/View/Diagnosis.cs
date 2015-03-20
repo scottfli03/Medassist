5b984707ff4dc264d24505cb0efbc6da4eac5414
Jon Greene <jgreene1@my.westga.edu>
2015-03-19 21:43:52 -0400

    Created initial view files and folders

---
+++ MedAssist/View/Diagnosis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedAssist.View
+{
+    public partial class Diagnosis : Form
+    {
+        public Diagnosis()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Diagnosis_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MedAssist/View/FullReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedAssist.View
+{
+    public partial class FullReport : Form
+    {
+        public FullReport()
+        {
+            InitializeComponent();
+        }
+
+        private void FullReport_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
