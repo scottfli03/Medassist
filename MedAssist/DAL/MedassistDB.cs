f5e83c9274a55f7449185a2c589a244b6d724925
Scott Flischel <sflisch1@my.westga.edu>
2015-03-26 02:30:53 -0400

    Finished model by adding: Employee, Patient, Test, and Order.  Also created connection called MedassistDB.cs.  I connected through localhost this semester you may have to use localhost/SQLEXPRESS or something along those lines.

---
+++ MedAssist/DAL/MedassistDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace MedAssist.DAL
+{
+    /// <summary>
+    /// Gets a connection to the Medassist Database
+    /// </summary>
+    class MedassistDB
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString = "Data Source=localhost; Initial Catalog=cs6232-g5;" +
+                "Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
---
+++ MedAssist/Model/Order.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedAssist.Model
+{
+    class Order
+    {
+        private int orderID;
+        private DateTime orderDate;
+        private DateTime? datePerformed;
+        private Boolean? result;
+        private int testID;
+        private int visitID;
+
+        public Order() { }
+
+        public int OrderID
+        {
+            get
+            {
+                return orderID;
+            }
+            set
+            {
+                orderID = value;
+            }
+        }
+
+        public DateTime OrderDate
+        {
+            get
+            {
+                return orderDate;
+            }
+            set
+            {
+                orderDate = value;
+            }
+        }
+
+        public DateTime? DatePerformed
+        {
+            get
+            {
+                return datePerformed;
+            }
+            set
+            {
+                datePerformed = value;
+            }
+        }
+
+        public Boolean? Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+            }
+        }
+
+        public int TestID
+        {
+            get
+            {
+                return testID;
+            }
+            set
+            {
+                testID = value;
+            }
+        }
+
+        public int VisitID
+        {
+            get
+            {
+                return visitID;
+            }
+            set
+            {
+                visitID = value;
+            }
+        }
+    }
+}
